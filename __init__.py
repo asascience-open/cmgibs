26c3508d6dc171a2dbbe6072118934086df5bd03
daltonkell <daltonkell13@gmail.com>
2018-07-06 17:18:02 -0400

    Fixed typos, naming conventions, and committed the pickle

--- __init__.py
+++ __init__.py
@@ -1,8 +1,8 @@
 
-from .cmgibs import GibsColormap
+from gibs import GibsColormap
 
 # sst = NasaColormap('GHRSST_L4_G1SST_Sea_Surface_Temperature.xml').cmap
 
-from ._version import get_versions
+from _version import get_versions
 __version__ = get_versions()['version']
-del get_versions
+# del get_versions
--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 import versioneer
-setup(name='cmnasa',
+from distutils.core import setup
+
+setup(name='cmgibs',
       version=versioneer.get_version(),
       cmdclass=versioneer.get_cmdclass(),
       author='Dalton R. Kell/Brian McKenna',
+      author_email=['dalton.kell@rpsgroup.com', 'brian.mckenna@rpsgroup.com'],
       description='NASA colormaps powered by Python',
-      long_description=open(README.md).read(),
-      install_requries=[
-          'lxml==4.2.3',
-          'matplotlib==2.2.2',
-          'numpy==1.14.5'
+      long_description=open('README.md').read(),
+      install_requires=[
+          'lxml>=4.2.3',
+          'matplotlib>=2.2.2',
+          'numpy>=1.14.5'
       ]
 )
